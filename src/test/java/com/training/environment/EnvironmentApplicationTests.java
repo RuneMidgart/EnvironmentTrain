d2144c14e4ace71a443eac2ead62b9fe0127d8d1
ZhaoZhen <runemidgart@icloud.com>
2022-07-10 20:40:12 +0800

    初始化

---
+++ src/test/java/com/training/environment/EnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
